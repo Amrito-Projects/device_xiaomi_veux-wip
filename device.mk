8bae1f0f51c23659cb6167822b061160ec0a17ff
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    veux: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

--- device.mk
+++ device.mk
@@ -486,6 +486,10 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
