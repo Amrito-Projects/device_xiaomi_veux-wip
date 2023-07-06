373cbce871892aa7287ac25fc1c00b4d5cee2dd8
nnippon99 <adamayyad1999@hotmail.com>
2023-07-06 19:23:48 +0300

    veux: switch to inbuilt Google dialer recorder by default
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>

--- lineage_veux.mk
+++ lineage_veux.mk
@@ -11,6 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_CALL_RECORDING_SUPPORTED := true
+
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
