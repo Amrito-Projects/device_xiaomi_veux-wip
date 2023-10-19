96b358d10df33125faa00651f25278ea54349c4d
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    veux: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device.mk
+++ device.mk
@@ -591,7 +591,8 @@ PRODUCT_PACKAGES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # Inherit from vendor blobs
 include vendor/xiaomi/veux/veux-vendor.mk
