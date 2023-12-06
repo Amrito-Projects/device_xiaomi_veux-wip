10638cc6b98c0c9f2e124eaa9a805d6e5b4db91f
Giovanni Ricca <giovanniricca@proton.me>
2023-12-06 09:50:13 +0100

    veux: Build Goodix interface from source
    Change-Id: Ibaf1b9489b509318032c720c9c83e315cfe6ab3e

--- device.mk
+++ device.mk
@@ -204,6 +204,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    com.fingerprints.extension@1.0.vendor \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # GPS
