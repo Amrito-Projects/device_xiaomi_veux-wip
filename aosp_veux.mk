f1adbdd0197a2313613a42599b8becdb10527486
Amrito Das / <amrito@myyahoo.com>
2024-10-14 15:28:52 +0530

    veux: Bringup @The-Pixel-Project

--- aosp_veux.mk
+++ aosp_veux.mk
@@ -8,16 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common The Pixel Project stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+# The Pixel Project flags
 TARGET_CALL_RECORDING_SUPPORTED := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_PIXEL_CHARGER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+TARGET_INCLUDE_CAMERA_GO := true
+CUSTOM_BUILD_TYPE := Official
 
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-PRODUCT_NAME := lineage_veux
+PRODUCT_NAME := aosp_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
